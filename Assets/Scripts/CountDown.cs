41cd7a0d9db458fb70438bc246ad43a352231fab
Luis Palomo <luispalomosiv@gmail.com>
2016-10-29 19:28:33 +0200

    Palomo commit

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class CountDown : MonoBehaviour {
+
+    public Image currentTimeBar;
+    public Text timeText;
+
+    private float currentTime;
+    private float maxTime;
+    private bool paused;
+
+	// Use this for initialization
+	void Start () {
+        maxTime = 2;
+        currentTime = maxTime;
+        paused = false;
+        timeText.text = maxTime.ToString();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!paused)
+        {
+            currentTime -= Time.deltaTime;
+            if(currentTime < 0)
+            {
+                currentTime = 0;
+                paused = true;
+                // TODO: Llamar GameOver
+            }
+            float ratio = currentTime / maxTime;
+
+            currentTimeBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+            timeText.text = (currentTime * 100).ToString("0");
+        }
+	}
+
+    public void restart()
+    {
+        maxTime -= maxTime * 0.1f * maxTime * 0.2f;
+        currentTime = maxTime;
+        paused = false;
+        timeText.text = maxTime.ToString();
+    }
+}
---
+++ Assets/Scripts/ProfilePhoto.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfilePhoto : MonoBehaviour {
+
+    float offsetX;
+    float initialX;
+    float initialY;
+
+    bool draggable;
+
+    // Use this for initialization
+    void Start () {
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+        draggable = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        offsetX = transform.position.x - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, transform.position.y, 21f)).x;
+    }
+
+    void OnMouseDrag()
+    {
+        if (draggable)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, transform.position.y, 21f));
+            transform.position = new Vector3(transform.position.x + offsetX, initialY, 0.0f);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        
+    }
+
+    void OnMouseUp()
+    {
+        transform.position = new Vector3(initialX, initialY, 0.0f);
+        draggable = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        draggable = false;
+        CountDown countdown = FindObjectOfType(typeof(CountDown)) as CountDown;
+        countdown.restart();
+        transform.position = new Vector3(initialX, initialY, 0.0f);
+        // TODO: Llamar renueva profile
+    }
+}
